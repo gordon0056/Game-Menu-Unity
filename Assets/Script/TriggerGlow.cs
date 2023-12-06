13ce9c1877dc0dd7ca164d3a3256c4e79de94e04
gordon0056 <gordon0056@users.noreply.github.com>
2023-12-07 00:09:57 +0300

    thirdCommit

--- Assets/Script/TriggerGlow.cs
+++ Assets/Script/TriggerGlow.cs
@@ -7,7 +7,7 @@ public class TriggerGlow : MonoBehaviour
     [SerializeField] private ParticleSystem _particleSystemContinue;
     [SerializeField] private ParticleSystem _particleSystemGiveUp;
     [SerializeField] private float _waitTimeContinue = 3f;
-    [SerializeField] private float _waitTimeGiveUp = 6f;
+    [SerializeField] private float _waitTimeGiveUp = 3f;
     private float _idleTimer = 0f;
     private bool _particlesActive = false;
 
@@ -15,32 +15,32 @@ public class TriggerGlow : MonoBehaviour
     {
         if (Input.touchCount > 0 || Input.GetMouseButton(0))
         {
-            // Если произошло взаимодействие с экраном, сбрасываем таймер и деактивируем анимацию частиц
             _idleTimer = 0f;
-            //DeactivateParticles();
+            DeactivateParticles(_particleSystemContinue);
+            DeactivateParticles(_particleSystemGiveUp);
+            
         }
         else
         {
-            // Если нет взаимодействия, увеличиваем таймер
-            _idleTimer += Time.fixedDeltaTime;
+            _idleTimer += Time.deltaTime;
             if (_idleTimer >= _waitTimeContinue && !_particlesActive)
             {
-                // Если время бездействия достигло 3 секунд и анимация частиц не активна, активируем ее
                 ActivateParticles(_particleSystemContinue);
-            }
-            if (_idleTimer >= _waitTimeGiveUp && !_particlesActive)
-            {
-                // Если время бездействия достигло 6 секунд и анимация частиц не активна, активируем ее
-                DeactivateParticles(_particleSystemContinue);
                 ActivateParticles(_particleSystemGiveUp);
             }
+            /*if (_idleTimer >= _waitTimeGiveUp && !_particlesActive)
+            {
+                DeactivateParticles(_particleSystemContinue);
+              
+            }
+            */
         }
     }
 
     private void ActivateParticles(ParticleSystem particle)
     {
         _particlesActive = true;
-        particle.gameObject.SetActive(_particlesActive);
+        //particle.gameObject.SetActive(_particlesActive);
         particle.Play();  
     }
 
@@ -48,6 +48,6 @@ public class TriggerGlow : MonoBehaviour
     {
         particle.Stop();
         _particlesActive = false;
-        particle.gameObject.SetActive(_particlesActive);
+        //particle.gameObject.SetActive(_particlesActive);
     }
 }
